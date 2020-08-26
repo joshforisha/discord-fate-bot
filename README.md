5b211a73545927155247981942dc4f43567a1dcc
Josh Forisha <josh@forisha.com>
2020-08-26 00:10:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fate Bot
+
+A Discord bot for playing the [Fate RPG](https://fate-srd.com)
+
+## Setup
+
+(TODO)
+
+## Commands
+
+(TODO)
---
+++ src/main.js
@@ -0,0 +1,124 @@
+require("dotenv").config();
+
+const { Client, MessageEmbed } = require("discord.js");
+
+const AspectType = {
+  Aspect: "ASPECT",
+  Boost: "BOOST",
+};
+
+const entities = [];
+
+const discordClient = new Client();
+
+process.on("SIGINT", () => {
+  discordClient.destroy();
+  console.log("Disconnected");
+  process.exit(0);
+});
+
+function addAspect(ent, type, name, freeInvokes) {
+  const e = entities.findIndex(({ name }) =>
+    name.toLowerCase().startsWith(ent)
+  );
+  if (e > -1) {
+    entities[e].aspects.push({ name, freeInvokes, type });
+    return true;
+  }
+
+  return false;
+}
+
+function aspectText({ freeInvokes, name, type }) {
+  const sym = type === AspectType.Boost ? "*" : "";
+  const fis = typeof freeInvokes !== "undefined" ? ` \`${freeInvokes}\`` : "";
+  return `> ${sym}${name}${sym}${fis}`;
+}
+
+function entitiesEmbed() {
+  return new MessageEmbed().addFields(
+    entities.map(({ aspects, name }) => ({
+      name,
+      value: aspects.length > 0 ? aspects.map(aspectText) : `*No aspects yet*`,
+    }))
+  );
+}
+
+discordClient.on("message", (message) => {
+  if (message.content.startsWith("|")) {
+    const tokens = message.content.split(" ");
+    switch (tokens[0]) {
+      case "|add": {
+        if (
+          addAspect(
+            tokens[1].toLowerCase(),
+            AspectType.Aspect,
+            tokens.slice(2).join(" ")
+          )
+        ) {
+          message.channel.send(entitiesEmbed());
+        } else {
+          message.channel.send(
+            `I couldn't find an entity starting with "${tokens[1]}"`
+          );
+        }
+        break;
+      }
+
+      case "|add1": {
+        if (
+          addAspect(
+            tokens[1].toLowerCase(),
+            AspectType.Aspect,
+            tokens.slice(2).join(" "),
+            1
+          )
+        ) {
+          message.channel.send(entitiesEmbed());
+        } else {
+          message.channel.send(
+            `I couldn't find an entity starting with "${tokens[1]}"`
+          );
+        }
+        break;
+      }
+
+      case "|add2": {
+        if (
+          addAspect(
+            tokens[1].toLowerCase(),
+            AspectType.Aspect,
+            tokens.slice(2).join(" "),
+            2
+          )
+        ) {
+          message.channel.send(entitiesEmbed());
+        } else {
+          message.channel.send(
+            `I couldn't find an entity starting with "${tokens[1]}"`
+          );
+        }
+        break;
+      }
+
+      case "|help":
+        message.channel.send(
+          new MessageEmbed().addFields({
+            name: "Commands",
+            value: ["`|add *<entity>* *<aspect>*`", "`|new *<entity>*`"],
+          })
+        );
+        break;
+
+      case "|new":
+        entities.push({ aspects: [], name: tokens.slice(1).join(" ") });
+        message.channel.send(entitiesEmbed());
+        break;
+
+      default:
+        message.channel.send(`I didn't understand the command "${tokens[0]}"`);
+    }
+  }
+});
+
+discordClient.login(process.env["BOT_TOKEN"]);
